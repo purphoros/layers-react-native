e0f076f4f235bf1a72fbd3414ac2d561c647b2e5
purphoros <4297636+purphoros@users.noreply.github.com>
2022-04-24 15:28:48 +1000

    Update: 24/04/2022 15:28:48

---
+++ dist/esm/types/components/Context.d.ts
@@ -0,0 +1,30 @@
+import React, { FC, ReactElement } from 'react';
+export declare const MINIMIZED_FLAG = 0;
+export declare const MAXIMIZED_FLAG = 1;
+interface Base {
+    layerUuid?: string;
+    updatedOn?: number;
+    name?: string;
+    orientation?: string;
+    component: ReactElement<LayerProps>;
+}
+export interface LayerProps {
+    layerUuid?: string;
+    style?: any;
+}
+export interface Layer extends Base {
+    layerUuid: string;
+}
+export interface Context {
+    layers: Array<Layer>;
+    createLayer: (values: Base, callback?: () => void) => void;
+    closeLayerByUuid: (layerUuid?: string) => void;
+    closeLayerByName: (name: string) => void;
+    closeAllLayers: () => void;
+}
+export declare const useLayer: () => Context;
+interface Props {
+    children: React.ReactNode;
+}
+export declare const LayersProvider: FC<Props>;
+export {};
