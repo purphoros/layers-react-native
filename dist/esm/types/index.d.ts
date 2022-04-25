4d65b09a4eddf35096e94f44c451056adc2b6e33
purphoros <4297636+purphoros@users.noreply.github.com>
2022-04-26 08:37:54 +1000

    Update: 26/04/2022 08:37:54

--- dist/esm/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { LayersProvider } from './components/Context';
-export { useLayer } from './components/Context';
-export { Context } from './components/Context';
-export { Layer } from './components/Context';
-export { LayerProps } from './components/Context';
